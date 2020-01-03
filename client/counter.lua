16ab79c8dd3019513ccb2c8ec72a8aad023f9cb1
DKFN <valere.tetelin@gmail.com>
2020-01-03 21:29:51 +0100

    Package name now match Onset naming convention

--- client/counter.lua
+++ client/counter.lua
@@ -1,7 +1,7 @@
 local testUi
 AddEvent("OnPlayerSpawn", function()
     testUi = CreateWebUI(0.0, 0.0, 0.0, 0.0, 5, 60)
-	LoadWebFile(testUi, "http://asset/onset-react-demo/react_ts_webpack/build/index.html")
+	LoadWebFile(testUi, "http://asset/onset_react_example/react_ts_webpack/build/index.html")
 	SetWebAlignment(testUi, 0.0, 0.0)
 	SetWebAnchors(testUi, 0.0, 0.0, 1.0, 1.0)
     SetWebVisibility(testUi, WEB_VISIBLE)
